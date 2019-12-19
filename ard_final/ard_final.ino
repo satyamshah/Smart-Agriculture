927840a3515e1cf8513c98cbb464fe4078c745b9
BedaBora <42554809+BedaBora@users.noreply.github.com>
2019-12-20 01:40:31 +0800

    Update ard_final.ino

--- ard_final/ard_final.ino
+++ ard_final/ard_final.ino
@@ -1,18 +1,20 @@
-#include <dht.h>
-#include <SoftwareSerial.h>
-#include <LiquidCrystal_I2C.h>
-SoftwareSerial mySerial(10, 11);
+#include <dht.h>                          //DHT library
+#include <SoftwareSerial.h>               
+#include <LiquidCrystal_I2C.h>            //LCD Library
+
+SoftwareSerial mySerial(10, 11);          //Defininig serial port
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 dht DHT;  
 #define DHT22PIN A3
-int ack, soil, light;
+int ack, soil, light, motor=0;
+
 void setup()
 {
   Serial.begin(9600);
   mySerial.begin(9600);
-  pinMode(A0, INPUT);
-  //pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  pinMode(A0, INPUT);                   //Input for light sensor
+  pinMode(A1, INPUT);                   //Input for soil moisture sensor
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
@@ -22,36 +24,43 @@ void setup()
   delay(2000);
   lcd.clear();
 }
+
 void loop()
 {
   ack = 0;
   int chk = DHT.read22(DHT22PIN);
-  /*switch (chk)
+
+  //------------------------Check to see if DHT is working-------------------------//
+  switch (chk)
   {
     case DHTLIB_ERROR_CONNECT:
       ack = 1;
       break;
-  }*/
+  }
+  //------------------------------------------------------------------------------//
 
-  //soil=analogRead(A0);
-    light=analogRead(A1);
+    light=analogRead(A1);                    //Reading light sensor data
     light=map(light, 0, 1023, 100, 0);
-    soil=analogRead(A0);
-    soil=map(soil, 0, 1023, 0, 100);
+    
+    soil=analogRead(A0);                    //Reading soil moisture data
+    soil=map(soil, 0, 1023, 100, 0);
+    
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Soil:");
-    //soil = map(soil, 0, 1023, 100, 0);
+    
+    lcd.print("Soil:");                   //Display soil data
     lcd.print(soil);
     lcd.print("%");
+    
     lcd.setCursor(0, 1);
-    lcd.print("Light:");
-    //light = map(light, 0, 1023, 0, 100);
+    lcd.print("Light:");                    //Display Light intensity
     lcd.print(light);
     lcd.print("%");
+    
     delay(3000);
     lcd.clear();
-    lcd.setCursor(0, 0);
+    
+    lcd.setCursor(0, 0);                      //Display dht data
     lcd.print("Temp:");
     lcd.print(DHT.temperature);
     lcd.print(" *C");
@@ -60,27 +69,37 @@ void loop()
     lcd.print(DHT.humidity);
     lcd.print("%");
     delay(3000);
+
+    
   if (ack == 0)
   {
     Serial.print("Temperature(*C) = ");
     Serial.println(DHT.temperature, 0);
     Serial.print("Humidity(%) = ");
     Serial.println(DHT.humidity, 0);
-    Serial.print("Soil(%) =");
+    Serial.print("Soil(%) = ");
     Serial.println(soil);
-    Serial.print("Light =");
+    Serial.print("Light = ");
     Serial.println(light);
-    Serial.println("-------------------------");
-    //------Sending Data to ESP8266--------//
+    Serial.println("-------------------------------");
+
+    
+    //-------------------Sending Data to ESP8266----------------//
     mySerial.print('*'); // Starting char
     mySerial.print(DHT.temperature, 0); //2 digit data
     mySerial.print(DHT.humidity, 0); //2 digit data
     mySerial.print(light);
     mySerial.print(soil);
     mySerial.println('#'); // Ending char
-    //------------------------------------//
-    delay(2000);
+    //---------------------------------------------------------//
+
+
+    //-------------------------Reading Data from Blynk--------------------------//
+      
+    
+    //-------------------------------------------------------------------------//
   }
+  
   if (ack == 1)
   {
     Serial.print("NO DATA");
